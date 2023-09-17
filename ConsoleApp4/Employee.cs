707eda3b76705bc335c48cf2cf441c328d7f765f
totoqqwe <anton.stepanchenko14@gmail.com>
2023-09-17 17:17:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Employee
+    {
+
+        private string name;
+        private string surname;
+
+        public Employee(string name, string surname)
+        {
+            this.name = name;
+            this.surname = surname;
+        }
+
+        public void CalculateSalaryAndTax(string position, int experience)
+        {
+            double baseSalary = 0.0;
+            double tax = 0.0;
+
+            switch (position.ToLower())
+            {
+                case "manager":
+                    baseSalary = 3000.0;
+                    break;
+                case "developer":
+                    baseSalary = 4000.0;
+                    break;
+                case "designer":
+                    baseSalary = 3500.0;
+                    break;
+            }
+
+            if (experience >= 3)
+            {
+                baseSalary += 500.0;
+            }
+
+            tax = baseSalary * 0.1;
+
+            Console.WriteLine("Інформація про співробітника:");
+            Console.WriteLine($"Прізвище: {name}");
+            Console.WriteLine($"Ім'я: {surname}");
+            Console.WriteLine($"Посада: {position}");
+            Console.WriteLine($"Оклад: {baseSalary:C2}");
+            Console.WriteLine($"Податковий збір: {tax:C2}");
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Джо", "Наваро");
+
+            employee.CalculateSalaryAndTax("developer", 5);
+        }
+    }
+}
